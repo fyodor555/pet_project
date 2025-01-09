807560a7c742c3b1546e136864dacdd079cf3745
fyodor555 <95444945+fyodor555@users.noreply.github.com>
2025-01-09 21:57:15 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,28 +1,25 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from typing import Optional
-
+from datetime import date, time
 
 engine = create_async_engine(
-    "sqlite+aiosqlite:///persons.db"
+    "sqlite+aiosqlite:///tasks.db"
 )
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
-
 
 class Model(DeclarativeBase):
     pass
 
 
-class PersonTable(Model):
-    __tablename__ = "persons"
+class TaskTable(Model):
+    __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    surname: Mapped[str]
-    post: Mapped[Optional[str]]
-    age: Mapped[Optional[int]]
+    title: Mapped[str]
     discription: Mapped[Optional[str]]
+    image_adress: Mapped[Optional[str]]
 
 
 async def create_tables():
